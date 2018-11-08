5261b3c21ae73d26023b03d08621fd625c951970
Antonio Gil <antonioluisgil@gmail.com>
2018-11-08 11:20:53 +0100

    Add example 04-nodemon

---
+++ 04-nodemon/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const http = require("http");
+const routes = require("./routes");
+
+const server = http.createServer(routes.handler);
+
+server.listen(3000, () => {
+  console.log("Listen on port 3000");
+});
