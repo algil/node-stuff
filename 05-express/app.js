697a7f96ab87cac37ccd028cf7a73974184bf066
Antonio Gil <antonioluisgil@gmail.com>
2018-11-12 20:14:50 +0100

    Add express example

---
+++ 05-express/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const express = require("express");
+
+const app = express();
+
+// Adding a middleware
+app.use((req, res, next) => {
+  console.log("Hello from first middleware!");
+  next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware!");
+  res.send("<h1>Hello from Express</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("Listen on port 3000");
+});
