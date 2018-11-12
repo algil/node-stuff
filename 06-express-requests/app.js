9613f8ffe509f09f121b4c344cc2b2026578bf0c
Antonio Gil <antonioluisgil@gmail.com>
2018-11-12 20:23:39 +0100

    Add "parsing incoming request" with express

---
+++ 06-express-requests/app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded());
+
+app.use("/products", (req, res, next) => {
+  res.send(`
+    <form method="POST" action="/add-product">
+      <input name="product">
+      <button>Add Product</button>
+    </form>
+  `);
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from express</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("Listen on port 3000");
+});
